50c6090fb0eadf221095a8e97c24c2b9d9ae1ac5
Plínio César Simões Fernandes <pliniocsfernandes@gmail.com>
2013-08-12 12:33:52 -0300

    Created specific models for the categories.
    These models store the apropriate phrases for each category.
    Through inheritance, they use the logic of the Phrases model for selecting a phrase.

---
+++ model/GuruPhrases.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__."/Phrases.php";
+
+class GuruPhrases extends Phrases {
+    function __construct() {
+        $this->phrases = array(
+            "When one follows conclusion and enlightenment, one is able to hurt politics.",
+            "When one respects mind and enlightenment, one is able to love beauty.",
+            "One must believe the lama in order to receive the master of imminent affirmation.",
+            "Who can experience the blessing and booda-hood of a doer if he has the searious peace of the self.",
+            "Synthetic zens views most harmonies.",
+            "Who can understand the afterlife and booda-hood of a source if he has the important extend of the lotus.",
+            "Be occult for whoever listens, because each has been absorbed with relativity.",
+            "One must believe the yogi in order to absorb the saint of separate control.",
+            "One must develop the master in order to remember the explosion of the courage of ultimate advice!",
+            "Who can trap the heaven and surrender of a master if he has the wonderful history of the guru."
+        );
+    }
+}
---
+++ model/PiratePhrases.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__."/Phrases.php";
+
+class PiratePhrases extends Phrases {
+    function __construct() {
+        $this->phrases = array(
+            "Whine calmly like a big cannon.",
+            "Rainy, coal-black planks unlawfully hoist a gutless, big shore.",
+            "The jolly roger sings urchin like a rainy jack.",
+            "All bilge rats rob undead, coal-black furners.",
+            "The cannibal fights with greed, raid the fortress until it falls.",
+            "When the anchor screams for port degas, all suns endure proud, stormy lagoons.",
+            "Ho-ho-ho! fortune of yellow fever.",
+            "Aww, c'mon.",
+            "Fine fights lead to the courage.",
+            "Cannons wave with endurance at the dead la marsa beach!"
+        );
+    }
+}
